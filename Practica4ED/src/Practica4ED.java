ffd71deabd367581a9de2e4c7ef3d25ac9285591
ADOLFO <ADOLFO@DESKTOP-9V4G0J1>
2018-03-02 17:50:32 +0100

    Voy a subir mi proyecto a GitHub.

---
+++ Practica4ED/src/Practica4ED.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Practica4ED
+{
+
+	public static void main(String[] args) throws NumberFormatException, IOException
+	{
+		int numero1, numero2, comodin;
+		
+		BufferedReader lectura = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("dame el número 1: ");
+		numero1 = Integer.parseInt(lectura.readLine());
+		System.out.println("Dame el número 2: ");
+		numero2 = Integer.parseInt(lectura.readLine());
+		
+		comodin = numero2;
+		numero2 = numero1;
+		numero1 = comodin;
+	}
+	//Comentario realizado a través de commit.
+}
