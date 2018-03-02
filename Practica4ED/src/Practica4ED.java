2bfd1eb161bb7381db676d00b80fb7fcfa6478d3
Felix del Rio <35599432+felisito16@users.noreply.github.com>
2018-03-02 18:52:24 +0100

    Update Practica4ED.java
    Comentario en la linea 7

--- Practica4ED/src/Practica4ED.java
+++ Practica4ED/src/Practica4ED.java
@@ -4,20 +4,20 @@ import java.io.InputStreamReader;
 
 public class Practica4ED
 {
-
+	// Soy Felix y te estoy dejando un comentario :D
 	public static void main(String[] args) throws NumberFormatException, IOException
 	{
 		int numero1, numero2, comodin;
 		
 		BufferedReader lectura = new BufferedReader(new InputStreamReader(System.in));
-		System.out.println("dame el número 1: ");
+		System.out.println("dame el nÃºmero 1: ");
 		numero1 = Integer.parseInt(lectura.readLine());
-		System.out.println("Dame el número 2: ");
+		System.out.println("Dame el nÃºmero 2: ");
 		numero2 = Integer.parseInt(lectura.readLine());
 		
 		comodin = numero2;
 		numero2 = numero1;
 		numero1 = comodin;
 	}
-	//Comentario realizado a través de commit.
+	//Comentario realizado a travÃ©s de commit.
 }
